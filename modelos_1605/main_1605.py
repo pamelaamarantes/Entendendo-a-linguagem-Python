de6cff28bbf6e04bcdd30ec0d40109a6c6b92221
pamelaamarantes <128612558+pamelaamarantes@users.noreply.github.com>
2024-05-17 09:42:27 -0300

    Atividades_interligadas2

---
+++ modelos_1605/main_1605.py
@@ -0,0 +1,9 @@
+# Crie um arquivo chamado main.py, importe a classe Livro e, no arquivo main.py, instancie dois objetos da classe Livro e exiba a mensagem formatada utilizando o método str.
+
+from Livros_1605 import Livro  
+
+Main_Livro1 = Livro('O fabricante de lágrimas', 'Erin Doom', 2023)
+Main_Livro2 = Livro('Minha Culpa', 'Mercedes Ron', 2020)
+
+print(Main_Livro1)
+print(Main_Livro2)
